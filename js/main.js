36ba8e8fa926a9af72399d6495f187942242e6b9
Mason <chtuff@gmail.com>
2023-11-15 20:27:10 -0500

    updating files

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ function getDrink(){
         .then(res => res.json()) // parse response as JSON
         .then(data => {
          console.log(data.drinks)
-        
-            // data.drinks.forEach(obj => console.log(obj.name))
+            const item = new DrinkInfo(data.drinks[])
+            item.testCall()
+            data.drinks.forEach(obj => console.log(obj.name))
             document.querySelector('h3').innerText = data.drinks[0].strDrink
             document.querySelector('img').src = data.drinks[0].strDrinkThumb
             document.querySelector('span').innerText = data.drinks[0].strInstructions
-            document.querySelector('ul').innerText = data.drinks[0].strIngredient1
+            document.querySelector('table').innerText = data.drinks[0].strIngredient1
         
           
         })
@@ -23,4 +24,17 @@ function getDrink(){
         });
         
 }
+    class DrinkInfo {
+        constructor(drinkData){
+            this.name = drinkData.strDrink
+            this.ingredients = drinkData.strIngredient
+            this.image = drinkData.strDrinkThumb
+            this.instructions = drinkData.strInstructions
+        }
+
+        testCall(){
+            console.log(this.ingredients)
+        }
+
+    }
 
