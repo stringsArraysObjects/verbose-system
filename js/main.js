0a81b6e3b109bf3c3b71d93a8d1278381b4c7c84
Chris <chtuff@gmail.com>
2023-11-21 19:39:11 -0600

    updating with ingredients displaying in the DOM

--- js/main.js
+++ js/main.js
@@ -17,18 +17,12 @@ function getDrink(){
             document.querySelector('h3').innerText = data.drinks[0].strDrink
             document.querySelector('img').src = data.drinks[0].strDrinkThumb
             document.querySelector('span').innerText = data.drinks[0].strInstructions
-           
-       
-            const DRINKINGREDIENTS_URL = (`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=${drinkID}`)
-            
-            fetch(DRINKINGREDIENTS_URL)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                document.querySelector('.ingredients').innerText
-         
-             })
-            
+            document.querySelector('#ingredients1').innerText = data.drinks[0].strIngredient1
+            document.querySelector('#ingredients2').innerText = data.drinks[0].strIngredient2
+            document.querySelector('#ingredients3').innerText = data.drinks[0].strIngredient3
+            document.querySelector('#ingredients4').innerText = data.drinks[0].strIngredient4
+            document.querySelector('#ingredients5').innerText = data.drinks[0].strIngredient5
+          
           
         })
         .catch(err => {
