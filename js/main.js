a8e399af47dead6314c417443ad6e9427fc29253
Chris <chtuff@gmail.com>
2023-11-27 18:43:27 -0500

    update

--- js/main.js
+++ js/main.js
@@ -5,29 +5,33 @@ document.querySelector('button').addEventListener('click', getDrink)
 function getDrink(){
     let drink = document.querySelector('input').value
     
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s&=${drink}`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
             
          console.log(data.drinks)
-           let drinkID = data.drinks[0].idDrink
+           let drinkID = data.drinks.idDrink
         //    let lastThreeNumsOfDrinkID = drinkID.slice(2)
             console.log(drinkID)
             // data.drinks.forEach(obj => console.log(obj.name))
-            document.querySelector('h3').innerText = data.drinks[0].strDrink
-            document.querySelector('img').src = data.drinks[0].strDrinkThumb
-            document.querySelector('span').innerText = data.drinks[0].strInstructions
-           
+            document.querySelector('h3').innerText = data.drinks.strDrink
+            document.querySelector('img').src = data.drinks.strDrinkThumb
+            document.querySelector('span').innerText = data.drinks.strInstructions
+            let drinkIngredients = document.querySelector('.ingredients').innerText
+            for(let key in data.drinks.strIngredients){
+                drinkIngredients.value += strIngredients[key]
+                    console.log(drinkIngredients[key])
+            }
        
-            const DRINKINGREDIENTS_URL = (`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=${drinkID}`)
+            // const DRINKINGREDIENTS_URL = (`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=${drinkID}`)
             
-            fetch(DRINKINGREDIENTS_URL)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                document.querySelector('.ingredients').innerText
+            // fetch(DRINKINGREDIENTS_URL)
+            // .then(res => res.json())
+            // .then(data => {
+            //     console.log(data)
+            //     document.querySelector('.ingredients').innerText
          
-             })
+            //  })
             
           
         })
