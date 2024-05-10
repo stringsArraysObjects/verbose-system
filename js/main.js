b610362343dd679ffeb684ff1465d6ec652a548b
Chris <chtuff@gmail.com>
2024-05-10 19:47:16 -0400

    hiding the sub-headings until drink info populates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 document.querySelector('button').addEventListener('click', getDrink)
-// const myCarouselElement = document.querySelector('#carouselExampleAutoplaying')
-
-// // Constructor to create an instance of Carousel
-// const carousel = new bootstrap.Carousel(myCarouselElement, {
-//   touch: true, //passed arguements
-// })     
+const hideHeadings = document.querySelector('h3')    
 
 function getDrink(){
     let drink = document.querySelector('input').value
+    hideHeadings.classList.toggle('hidden')
     
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json())
     .then(data => {
            console.log(data.drinks)
-           document.querySelector('h3').innerText = data.drinks[0].strDrink
+           document.querySelector('h2').innerText = data.drinks[0].strDrink
            document.querySelector('#newUserInput').src = data.drinks[0].strDrinkThumb
-           document.querySelector('span').innerText = data.drinks[0].strInstructions
+           document.querySelector('p').innerText = data.drinks[0].strInstructions
            document.querySelector('#ingredients1').innerText = data.drinks[0].strIngredient1
            document.querySelector('#ingredients2').innerText = data.drinks[0].strIngredient2
            document.querySelector('#ingredients3').innerText = data.drinks[0].strIngredient3
@@ -28,7 +24,6 @@ function getDrink(){
            data.drinks.forEach(obj => {
                 console.log(obj.strDrink)
             })
-            
         })
         .catch(err => {
             console.log(`error ${err}`)
