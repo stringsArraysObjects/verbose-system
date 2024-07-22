ce3f795aecf868e201acce21dc78544c0bebf588
Mason <chtuff@gmail.com>
2024-07-22 15:43:05 -0400

    There are many issues to fix with this app, working on making the code less WET and than work on web design starting with layout.

--- js/main.js
+++ js/main.js
@@ -11,19 +11,20 @@ function getDrink(){
             // data.drinks.forEach(data.drinks => {
                 
                 console.log(data.drinks)
-                for(let i = 0; i < data.drinks.length; i++ ){
+                //document.querySelector('.drinkNames').innerText += data.drinks[i].strDrink + ',  '
 
-                    document.querySelector('.drinkNames').innerText += data.drinks[i].strDrink
+                for(let i = 0; i < data.drinks.length; i++ ){
+                    document.querySelector('h2').innerText = data.drinks[i].strDrink
                     document.querySelector('#newUserInput').src = data.drinks[i].strDrinkThumb
                     document.querySelector('p').innerText = data.drinks[i].strInstructions
-                    document.querySelector('#ingredients1').innerText = data.drinks[i].strIngredient1
-                    document.querySelector('#ingredients2').innerText = data.drinks[i].strIngredient2
-                    document.querySelector('#ingredients3').innerText = data.drinks[i].strIngredient3
-                    document.querySelector('#ingredients4').innerText = data.drinks[i].strIngredient4
-                    document.querySelector('#ingredients5').innerText = data.drinks[i].strIngredient5
-                    document.querySelector('#ingredients6').innerText = data.drinks[i].strIngredient6
-                    document.querySelector('#ingredients7').innerText = data.drinks[i].strIngredient7
-                    document.querySelector('#ingredients8').innerText = data.drinks[i].strIngredient8
+                    document.querySelector('ol').innerText += data.drinks[i].strIngredient1
+                    // document.querySelector('#ingredients2').innerText = data.drinks[i].strIngredient2
+                    // document.querySelector('#ingredients3').innerText = data.drinks[i].strIngredient3
+                    // document.querySelector('#ingredients4').innerText = data.drinks[i].strIngredient4
+                    // document.querySelector('#ingredients5').innerText = data.drinks[i].strIngredient5
+                    // document.querySelector('#ingredients6').innerText = data.drinks[i].strIngredient6
+                    // document.querySelector('#ingredients7').innerText = data.drinks[i].strIngredient7
+                    // document.querySelector('#ingredients8').innerText = data.drinks[i].strIngredient8
                 
                 }
 
