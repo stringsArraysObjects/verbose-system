f7f7008d7569a77f4e8e0739fc26eb8d7c883c62
Mason <chtuff@gmail.com>
2024-07-23 17:50:33 -0400

    this app is cursed the API is challenging to manipulate. Want to add a component that populates with all the drinks available based on a user only inputing a type of liquor if more than one is available. the mobile screen size looks much better now

--- js/main.js
+++ js/main.js
@@ -10,22 +10,26 @@ function getDrink(){
                 
                 console.log(data.drinks)
 
-                for(let prop in data.drinks){
-
-                    document.querySelector('h2').innerText = data.drinks[prop].strDrink
-                    document.querySelector('.cocktailPic').src = data.drinks[prop].strDrinkThumb
-                    document.querySelector('.drinkInstructions').innerText = data.drinks[prop].strInstructions
-                    document.querySelector('#ingredients1').innerText = data.drinks[prop].strIngredient1 
-                    document.querySelector('#ingredients2').innerText = data.drinks[prop].strIngredient2
-                    document.querySelector('#ingredients3').innerText = data.drinks[prop].strIngredient3
-                    document.querySelector('#ingredients4').innerText = data.drinks[prop].strIngredient4
-                    document.querySelector('#ingredients5').innerText = data.drinks[prop].strIngredient5
-                    document.querySelector('#ingredients6').innerText = data.drinks[prop].strIngredient6
-                  //   document.querySelector('#ingredients7').innerText = data.drinks[i].strIngredient7
-                  //   document.querySelector('#ingredients8').innerText = data.drinks[i].strIngredient8
+                for(let element in data.drinks){
+                    
+                        document.querySelector('h2').innerText = data.drinks[element].strDrink 
+                        document.querySelector('.cocktailPic').src += data.drinks[element].strDrinkThumb
+                        document.querySelector('.drinkInstructions').innerText = data.drinks[element].strInstructions
+                        document.querySelector('#ingredients1').innerText = data.drinks[element].strIngredient1 
+                        document.querySelector('#ingredients2').innerText = data.drinks[element].strIngredient2
+                        document.querySelector('#ingredients3').innerText = data.drinks[element].strIngredient3
+                        document.querySelector('#ingredients4').innerText = data.drinks[element].strIngredient4
+                        document.querySelector('#ingredients5').innerText = data.drinks[element].strIngredient5
+                        document.querySelector('#ingredients6').innerText = data.drinks[element].strIngredient6
+                       document.querySelector('#ingredients7').innerText = data.drinks[element].strIngredient7
+                       document.querySelector('#ingredients8').innerText = data.drinks[element].strIngredient8
                 }
 
-            // });
+               
+                                
+              
+
+            
                 })
         .catch(err => {
             console.log(`error ${err}`)
