0825a53fc941c3a807f18b5c7930708b1b87d657
Mason <chtuff@gmail.com>
2024-08-17 14:12:06 -0400

    made the functions async and using the more modern syntax for the fetch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const skeleton =  document.querySelector('.skeleton')
+const hideHeadings = document.querySelector('section') 
 
 document.addEventListener("readystatechange", (event) => {
         if (event.target.readyState === "loading") {
@@ -6,45 +7,65 @@ document.addEventListener("readystatechange", (event) => {
           initLoader();
         } else if (event.target.readyState === "complete") {
             skeleton.remove()
+             getData()
+            hideHeadings.classList.toggle('hidden')
         }
 });
 
 document.querySelector('button').addEventListener('click', getDrink)
-const hideHeadings = document.querySelector('section')   
-function getDrink(){
+async function getDrink() {
     let drink = document.querySelector('input').value
-    hideHeadings.classList.toggle('hidden')
-    
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-    .then(res => res.json())
-    .then(data => {
-                
-                console.log(data.drinks)
-
-                for(let element in data.drinks){
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      for(let element in data.drinks){
                     
-                        document.querySelector('h2').innerText = data.drinks[element].strDrink 
-                        document.querySelector('.cocktailPic').src = data.drinks[element].strDrinkThumb
-                        document.querySelector('.drinkInstructions').innerText = data.drinks[element].strInstructions
-                        document.querySelector('#ingredients1').innerText = data.drinks[element].strIngredient1 
-                        document.querySelector('#ingredients2').innerText = data.drinks[element].strIngredient2
-                        document.querySelector('#ingredients3').innerText = data.drinks[element].strIngredient3
-                        document.querySelector('#ingredients4').innerText = data.drinks[element].strIngredient4
-                        document.querySelector('#ingredients5').innerText = data.drinks[element].strIngredient5
-                        document.querySelector('#ingredients6').innerText = data.drinks[element].strIngredient6
-                       document.querySelector('#ingredients7').innerText = data.drinks[element].strIngredient7
-                       document.querySelector('#ingredients8').innerText = data.drinks[element].strIngredient8
-                }
-
-               
-                                
-              
-
-            
-                })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
-        
-
+        document.querySelector('h2').innerText = data.drinks[element].strDrink 
+        document.querySelector('.cocktailPic').src = data.drinks[element].strDrinkThumb
+        document.querySelector('.drinkInstructions').innerText = data.drinks[element].strInstructions
+        document.querySelector('#ingredients1').innerText = data.drinks[element].strIngredient1 
+        document.querySelector('#ingredients2').innerText = data.drinks[element].strIngredient2
+        document.querySelector('#ingredients3').innerText = data.drinks[element].strIngredient3
+        document.querySelector('#ingredients4').innerText = data.drinks[element].strIngredient4
+        document.querySelector('#ingredients5').innerText = data.drinks[element].strIngredient5
+        document.querySelector('#ingredients6').innerText = data.drinks[element].strIngredient6
+       document.querySelector('#ingredients7').innerText = data.drinks[element].strIngredient7
+       document.querySelector('#ingredients8').innerText = data.drinks[element].strIngredient8
 }
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }  
+
+
+async function getData() {
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      document.querySelector('h2').innerText = data.drinks[0].strDrink 
+      document.querySelector('.cocktailPic').src = data.drinks[0].strDrinkThumb
+      document.querySelector('.drinkInstructions').innerText = data.drinks[0].strInstructions
+        document.querySelector('#ingredients1').innerText = data.drinks[0].strIngredient1 
+        document.querySelector('#ingredients2').innerText = data.drinks[0].strIngredient2
+        document.querySelector('#ingredients3').innerText = data.drinks[0].strIngredient3
+        document.querySelector('#ingredients4').innerText = data.drinks[0].strIngredient4
+        document.querySelector('#ingredients5').innerText = data.drinks[0].strIngredient5
+        document.querySelector('#ingredients6').innerText = data.drinks[0].strIngredient6
+       document.querySelector('#ingredients7').innerText = data.drinks[0].strIngredient7
+       document.querySelector('#ingredients8').innerText = data.drinks[0].strIngredient8
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
